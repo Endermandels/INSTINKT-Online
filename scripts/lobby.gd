171f089e49326c00cb41adf769164681559f05c2
Endermandels <elijahdelavar@gmail.com>
2025-01-09 13:45:12 -0800

    Add multiplayer HUD, sync facing and animations (badly)

--- scripts/lobby.gd
+++ scripts/lobby.gd
@@ -7,16 +7,21 @@ var peer = ENetMultiplayerPeer.new()
 const PORT = 4000
 const IP_ADDRESS = "localhost"
 
-@onready var host := $Host
-@onready var join := $Join
+@onready var host := $MultiplayerHUD/Panel/VBoxContainer/Host
+@onready var join := $MultiplayerHUD/Panel/VBoxContainer/Join
+@onready var multiplayer_hud := $MultiplayerHUD
+@onready var tile_map := $TileMapLayer
 
 func _ready():
+	tile_map.hide()
 	host.connect("pressed", _on_host_pressed)
 	join.connect("pressed", _on_join_pressed)
 
 func _on_join_pressed():
 	peer.create_client(IP_ADDRESS, PORT)
 	multiplayer.multiplayer_peer = peer
+	multiplayer_hud.hide()
+	tile_map.show()
 
 func _on_host_pressed():
 	peer.create_server(PORT)
@@ -28,3 +33,5 @@ func _add_player(id=1):
 	var player = player_scene.instantiate()
 	player.name = str(id)
 	call_deferred("add_child", player)
+	multiplayer_hud.hide()
+	tile_map.show()
