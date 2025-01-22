0dfec41995c8c8603b290b65c2c996d0eca18368
Endermandels <131790297+Endermandels@users.noreply.github.com>
2025-01-21 22:08:20 -0800

    World building (#4)
    * Start tile map atlasing
    
    * Add all tree tile atlas connections
    
    * Add collisions to terrains
    
    * Add y-sorting to obstacles and trees
    
    * Add decoration layer; Start tiling on next commit
    
    * Fill in island
    
    * Add more details to tiles
    
    * Start fixing tree tile atlas
    
    * Finally fix tree placement
    
    * Create first draft of world

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,12 +5,29 @@ class_name Lobby
 @onready var join := $MultiplayerHUD/Panel/VBoxContainer/Join
 @onready var ip_address_text := $MultiplayerHUD/Panel/IPAddress
 @onready var multiplayer_hud := $MultiplayerHUD
-@onready var tile_map := $TileMapLayer
+@onready var floor_tile_map := $Floor
+@onready var floor2_tile_map := $Floor2
+@onready var shadows_tile_map := $"Shadows"
+@onready var decoration_tile_map := $"Y-Sorted/Decoration"
+@onready var small_objects_tile_map := $"Y-Sorted/SmallObjects"
+@onready var tree_layer_1_tile_map := $"Y-Sorted/TreeLayer1"
+@onready var tree_layer_2_tile_map := $"Y-Sorted/TreeLayer2"
+@onready var tree_layer_3_tile_map := $"Y-Sorted/TreeLayer3"
 @onready var username := $MultiplayerHUD/Panel/Username
-@onready var players := $Players
+
+func _set_tile_map_layer_visibility(visibility: bool):
+	floor_tile_map.visible = visibility
+	floor2_tile_map.visible = visibility
+	decoration_tile_map.visible = visibility
+	shadows_tile_map.visible = visibility
+	small_objects_tile_map.visible = visibility
+	tree_layer_1_tile_map.visible = visibility
+	tree_layer_2_tile_map.visible = visibility
+	tree_layer_3_tile_map.visible = visibility
 
 func _ready():
-	tile_map.hide()
+	_set_tile_map_layer_visibility(false)
+	multiplayer_hud.show()
 	host.connect("pressed", _on_host_pressed)
 	join.connect("pressed", _on_join_pressed)
 
@@ -19,9 +36,9 @@ func _on_join_pressed():
 		ip_address_text.text = "localhost"
 	MultiplayerManager.join_host(ip_address_text.text, username.text)
 	multiplayer_hud.hide()
-	tile_map.show()
+	_set_tile_map_layer_visibility(true)
 
 func _on_host_pressed():
 	MultiplayerManager.become_host(username.text)
 	multiplayer_hud.hide()
-	tile_map.show()
+	_set_tile_map_layer_visibility(true)
